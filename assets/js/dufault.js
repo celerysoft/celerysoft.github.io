9a8025c728fbfd7f6e40642b03e515b305f34393
Celery Qin <celerysoft@gmail.com>
2015-12-01 17:00:09 +0800

    maek scrollToTop button as a separate html

--- assets/js/dufault.js
+++ assets/js/dufault.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
 		$('.post img').addClass('img-responsive');
 });
-
-// 返回顶部
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 100) {
-		$('.scroll-top').fadeIn(200);
-	} else {
-		$('.scroll-top').fadeOut(200);
-	}
-});
-$('.scroll-top').bind('click', function(e) {
-	e.preventDefault();
-	$('body, html').animate({
-		scrollTop: 0
-		}, 200);
-});
