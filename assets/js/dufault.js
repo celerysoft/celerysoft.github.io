98ed18975aaec4bf7a28a344b89b6a70f3ea6fb5
Celery Qin <celerysoft@gmail.com>
2015-12-03 09:21:06 +0800

    add mobile phone supporting

--- assets/js/dufault.js
+++ assets/js/dufault.js
@@ -1,31 +1,100 @@
 $(document).ready(function() {
-		$('.post img').addClass('img-responsive');
-		$('.page img').addClass('img-responsive');
+  $('.post img').addClass('img-responsive');
+  $('.page img').addClass('img-responsive');
 });
 
+/* 返回顶部按钮，开始 */
 $(window).scroll(function() {
-	if ($(this).scrollTop() == 0) {
-		$('.navbar').fadeIn(200);
-	}
+  if ($(this).scrollTop() > 100) {
+    $('.scroll-top').fadeIn(200);
+  } else {
+    $('.scroll-top').fadeOut(200);
+  }
+});
+
+$('.scroll-top').bind('click', function(e) {
+  e.preventDefault();
+  $('body, html').animate({
+    scrollTop: 0
+  }, 200);
+});
+/* 返回顶部按钮，结束 */
+
+/* 下滑滚动导航菜单，上滑显示导航菜单开始 */
+// pc端
+$(window).scroll(function() {
+  if ($(this).scrollTop() == 0) {
+    $('.navbar').fadeIn(200);
+  }
 });
 
 var scrollFunc = function(e) {
-	e = e || window.event;
-	var position;
-	if (e.wheelDelta) {//IE/Opera/Chrome
-		position = e.wheelDelta;
-	} else if (e.detail) {//Firefox
-		position = e.detail;
-	}
-	if (position > 0) {
-		$('.navbar').fadeIn(200);
-	} else {
-		$('.navbar').fadeOut(200);
-	}
+  e = e || window.event;
+  var position;
+  if (e.wheelDelta) { //IE/Opera/Chrome
+    position = e.wheelDelta;
+  } else if (e.detail) { //Firefox
+    position = e.detail;
+  }
+  if (position > 0) {
+    $('.navbar').fadeIn(200);
+  } else {
+    var currentScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (currentScrollTop > 0) {
+      $('.navbar').fadeOut(200);
+    }
+  }
 }
 
-/*注册事件*/
-if(document.addEventListener) {
-	document.addEventListener('DOMMouseScroll',scrollFunc,false);
-}//W3C
-window.onmousewheel = document.onmousewheel = scrollFunc;//IE/Opera/Chrome
+/* 注册监听滚动事件 */
+window.addEventListener('DOMMouseScroll', scrollFunc, false); //W3C
+window.onmousewheel = document.onmousewheel = scrollFunc; //IE/Opera/Chrome
+
+// 移动端
+var startX = startY = 0;
+var mobileScrollTop = 0;
+
+function touchSatrtFunc(e) {
+  //e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+  var touch = e.touches[0]; //获取第一个触点
+  var x = Number(touch.pageX); //页面触点X坐标
+  var y = Number(touch.pageY); //页面触点Y坐标
+  startX = x;
+  startY = y;
+  mobileScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+}
+//touchmove事件
+function touchMoveFunc(e) {
+  //e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+  var touch = e.touches[0];
+  var x = touch.pageX - startX
+  var y = touch.pageY - startY;
+
+  var currentScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+  // 返回顶部按钮
+  if (currentScrollTop > 100) {
+    $('.scroll-top').fadeIn(200);
+  } else {
+    $('.scroll-top').fadeOut(200);
+  }
+  // Actionbar
+  if (currentScrollTop - mobileScrollTop > 20) {
+    $('.navbar-top').fadeIn(200);
+  } else if (currentScrollTop - mobileScrollTop < 20) {
+    $('.navbar-top').fadeOut(200);
+  }
+}
+
+//touchend事件
+function touchEndFunc(e) {
+  //e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+  var currentScrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+  var touch = e.changedTouches[0]; //获取第一个触点
+  var x = Number(touch.pageX); //页面触点X坐标
+  var y = Number(touch.pageY); //页面触点Y坐标
+}
+
+document.addEventListener('touchstart', touchSatrtFunc, false);
+document.addEventListener('touchmove', touchMoveFunc, false);
+document.addEventListener('touchend', touchEndFunc, false);
+/* 下滑滚动导航菜单，上滑显示导航菜单结束 */
